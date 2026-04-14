fe900ae7e62476a49d20dc16fa283ae94c6d7a96
Paulo Trajano <trajanopaulo10@gmail.com>
2026-04-15 01:04:06 +0800

    add a login form (#5)

---
+++ src/app/login/page.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { ADMIN } from '@/lib/constants'
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState<{ email?: string; password?: string }>({})
+  const router = useRouter()
+
+  const resetError = () => {
+    setError({ email: '', password: '' })
+  }
+
+  const validate = () => {
+    if (!email) {
+      setError((prev) => ({ ...prev, email: 'Email is required' }))
+    }
+
+    if (!password) {
+      setError((prev) => ({ ...prev, password: 'Password is required' }))
+    }
+
+    if (email && email !== ADMIN.email) {
+      setError((prev) => ({ ...prev, email: 'User does not exist' }))
+    } else if (password && password !== ADMIN.password) {
+      setError((prev) => ({ ...prev, password: 'Password is incorrect' }))
+    }
+
+    if (email === ADMIN.email && password === ADMIN.password) {
+      return true
+    }
+    return false
+  }
+
+  const handleLogin = () => {
+    resetError()
+    if (validate()) {
+      router.push('/admin')
+    }
+  }
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <form
+        className="flex flex-col items-center justify-center h-screen"
+        onSubmit={(e) => {
+          e.preventDefault()
+          handleLogin()
+        }}
+      >
+        <h1 className="text-4xl font-bold mb-8">Login</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          className="mb-4 px-4 py-2 border rounded w-64"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {error.email && (
+          <p className="text-sm text-red-100 mb-4 w-full">{error.email}</p>
+        )}
+        <input
+          type="password"
+          placeholder="Password"
+          className="mb-6 px-4 py-2 border rounded w-64"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error.password && (
+          <p className="text-sm text-red-100 mb-4 w-full">{error.password}</p>
+        )}
+        <button
+          type="submit"
+          className="px-6 py-2 bg-blue-600 cursor-pointer text-white rounded hover:bg-blue-700 transition w-full"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  )
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const ADMIN = {
+  email: 'admin',
+  password: 'secret',
+}
